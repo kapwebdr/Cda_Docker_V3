23709477159c4b7967678acce358ca8791782c77
kapwebdr <damien@kapweb.com>
2025-10-09 11:32:45 +0200

    dir project

--- src/Config/config.php
+++ src/Config/config.php
@@ -1,6 +1,16 @@
 <?php
-
+header('Content-Type: text/html; charset=utf-8'); 
+date_default_timezone_set("Europe/Paris");
+ini_set('memory_limit', '1G');
 
 define('DIR_SRC',__DIR__.'/../');
 define('DIR_APP',DIR_SRC.'App/');
+define('DIR_CONFIG',DIR_SRC.'Config/');
 define('DIR_PROJECTS',DIR_SRC.'Projects/');
+
+define('DIR_PROJECT',DIR_PROJECTS.PROJECT.'/');
+define('DIR_PROJECT_CONFIG',DIR_PROJECTS.PROJECT.'/Config');
+define('DIR_PROJECT_MODEL',DIR_PROJECTS.PROJECT.'/Model');
+define('DIR_PROJECT_CONTROLLER',DIR_PROJECTS.PROJECT.'/Controller');
+define('DIR_PROJECT_VIEWS',DIR_PROJECTS.PROJECT.'/Views');
+define('DIR_PROJECT_PUBLIC',DIR_PROJECTS.PROJECT.'/Public');
--- src/Projects/Altera/Public/index.php
+++ src/Projects/Altera/Public/index.php
@@ -1,3 +1,10 @@
 <?php
+$array_directories = explode('/',__DIR__);
+define('PROJECT',$array_directories[count($array_directories)-2]);
 require_once(__DIR__.'/../../../Config/config.php');
-echo 'Bonjour : '.DIR_PROJECTS;
+
+echo 'Bonjour : '.DIR_PROJECT;
+
+// echo '<pre>';
+// var_dump($array_directories);
+// echo '</pre>';
